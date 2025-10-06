20fefcf33c4bc9eb2b76f91c68b092221884b502
Rodrigo Rey <rodrigorey2005@gmail.com>
2025-10-05 21:17:00 -0300

    welcome page mobile

--- components/welcome-screen.tsx
+++ components/welcome-screen.tsx
@@ -48,42 +48,9 @@ export default function WelcomeScreen({ onEnterDesktop }: WelcomeScreenProps) {
       </div>
     </div>
 
-        {/* Mobile: dos filas, arriba KIKU y abajo CREAM, ocupando todo el ancho */}
-        <div className="md:hidden flex flex-col items-center justify-center leading-none pointer-events-none select-none w-full h-full">
-          <div className="flex items-end justify-between w-full">
-            {[
-              "/inicio celu/K CELU.svg",
-              "/inicio celu/I CELU.svg",
-              "/inicio celu/K CELU.svg",
-              "/inicio celu/U CELU.svg",
-            ].map((src, idx) => (
-              <img
-                key={src + idx}
-                src={src}
-                alt=""
-                draggable={false}
-                className="h-[18vh] sm:h-[20vh] w-auto"
-              />
-            ))}
-          </div>
-          <div className="mt-[2.5vw] flex items-end justify-between w-full">
-            {[
-              "/inicio celu/C CELU.svg",
-              "/inicio celu/R CELU.svg",
-              "/inicio celu/E CELU.svg",
-              "/inicio celu/A CELU.svg",
-              "/inicio celu/M CELU.svg",
-            ].map((src, idx) => (
-              <img
-                key={src + idx}
-                src={src}
-                alt=""
-                draggable={false}
-                className="h-[18vh] sm:h-[20vh] w-auto"
-              />
-            ))}
-          </div>
-        </div>
+        {/* Mobile: Nuevo layout personalizado */}
+        {/* Dejamos este contenedor vacío para mobile porque el layout móvil lo renderizamos fuera, con z más alto */}
+        <div className="md:hidden" />
       </div>
 
       {/* Desktop: CLICK / AQUI / CARITAS grandes centradas un poco más arriba del medio */}
@@ -96,7 +63,7 @@ export default function WelcomeScreen({ onEnterDesktop }: WelcomeScreenProps) {
           animate={{ rotate: clickTurns * 360 }}
           transition={{ duration: 0.8, ease: "easeInOut" }}
           whileHover={{ scale: 1.08 }}
-          className="select-none max-w-none h-[18vh] xl:h-[22vh] 2xl:h-[24vh] w-auto cursor-pointer drop-shadow-lg"
+          className="select-none max-w-none h-[18vh] xl:h-[22vh] 2xl:h-[24vh] w-auto cursor-pointer"
         />
           <motion.div
           className="relative cursor-pointer"
@@ -115,7 +82,7 @@ export default function WelcomeScreen({ onEnterDesktop }: WelcomeScreenProps) {
             draggable={false}
             whileHover={{ rotate: 360 }}
             transition={{ duration: 0.7, ease: "easeInOut" }}
-            className="select-none max-w-none h-[18vh] xl:h-[22vh] 2xl:h-[24vh] w-auto drop-shadow-lg mt-11 -ml-6"
+            className="select-none max-w-none h-[18vh] xl:h-[22vh] 2xl:h-[24vh] w-auto mt-11 -ml-6"
           />
         </motion.div>
         <div className="relative select-none">
@@ -123,45 +90,67 @@ export default function WelcomeScreen({ onEnterDesktop }: WelcomeScreenProps) {
             src="/inicio/CARITA.svg"
             alt="Caritas"
             draggable={false}
-            className="select-none max-w-none h-[12vh] xl:h-[16vh] 2xl:h-[18vh] w-auto drop-shadow-lg"
+            className="select-none max-w-none h-[12vh] xl:h-[16vh] 2xl:h-[18vh] w-auto"
           />
         </div>
       </div>
 
-      {/* Mobile: versión condensada (click no rota aquí) */}
-      <div className="md:hidden absolute top-3 left-1/2 -translate-x-1/2 z-[120] flex flex-row items-center pointer-events-auto">
-        <img
-          src="/inicio/CLICK.svg"
-          alt="Click"
-          draggable={false}
-          className="select-none max-w-none h-auto transform scale-75 -mr-4"
-        />
-        <motion.div
-          className="cursor-pointer"
-          onClick={onEnterDesktop}
-          initial={{ opacity: 0, scale: 0.95, rotate: 0 }}
-          animate={{ opacity: 1, scale: 1, rotate: 0 }}
-          whileHover={{ rotate: 360 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ duration: 0.7, ease: "easeInOut" }}
-          title="Entrar"
-          role="button"
-          aria-label="Entrar al escritorio"
-        >
-          <img
-            src="/inicio/AQUI.svg"
-            alt="Aquí"
-            draggable={false}
-            className="select-none max-w-none h-auto transform scale-75"
-          />
-        </motion.div>
-        <div className="select-none -ml-6 relative">
-          <img
-            src="/inicio/CARITA.svg"
-            alt="Caritas"
-            draggable={false}
-            className="select-none max-w-none h-auto transform scale-75"
-          />
+      {/* Mobile nuevo layout compuesto */}
+      <div className="md:hidden absolute inset-0 z-10 pointer-events-none select-none">
+        {/* KIKU + CLICK */}
+        <div className="relative w-full">
+          <div className="relative flex flex-row items-end gap-[1.2vw] mt-[24vw] ml-[2vw] mr-[10vw]">
+            {/* CLICK sobre KIKU más abajo y a la derecha */}
+            <img
+              src="/inicio/CLICK.svg"
+              alt="Click"
+              draggable={false}
+              className="absolute -top-[1vw] left-[26vw] h-[19vw] w-auto"
+            />
+            {/* KIKU con K e I sin separación */}
+            <div className="flex flex-row items-end">
+              <img src="/inicio celu/K.svg" alt="" draggable={false} className="h-[30vh] w-auto" />
+              <img src="/inicio celu/I.svg" alt="" draggable={false} className="h-[30vh] w-auto -ml-[1.5vw]" />
+            </div>
+            {/* resto de K U con gap mínimo */}
+            <img src="/inicio celu/KK.svg" alt="" draggable={false} className="h-[30vh] w-auto" />
+            <img src="/inicio celu/U.svg" alt="" draggable={false} className="h-[30vh] w-auto" />
+          </div>
+          {/* CREAM desplazado hacia la derecha */}
+          <div className="relative flex flex-row items-end gap-[1.2vw] justify-end w-full pr-[2vw]">
+            {["/inicio celu/C.svg", "/inicio celu/R.svg", "/inicio celu/E.svg", "/inicio celu/A.svg", "/inicio celu/M.svg"].map((src, idx) => (
+              <img
+                key={src + idx}
+                src={src}
+                alt=""
+                draggable={false}
+                className="h-[30vh] w-auto"
+              />
+            ))}
+          </div>
+          {/* AQUI superpuesto debajo de CREAM */}
+          <div className="relative w-full">
+            <motion.img
+              src="/inicio/AQUI.svg"
+              alt="Aquí"
+              draggable={false}
+              onClick={onEnterDesktop}
+              whileTap={{ scale: 0.95 }}
+              className="h-[13vh] w-auto mt-[-5vw] mx-auto cursor-pointer pointer-events-auto"
+            />
+          </div>
+          {/* CARITAS abajo a la derecha: tres caritas */}
+          <div className="flex flex-row items-end gap-[2vw] w-full justify-end pr-[6vw] mt-[5vw]">
+            {[1,2,3].map(i => (
+              <img
+                key={i}
+                src="/inicio/CARITA.svg"
+                alt="Caritas"
+                draggable={false}
+                className="h-[5vh] w-auto"
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
