f35f657a160d59dbb3dbc10a6db6823391233fcb
Rodrigo Rey <rodrigorey2005@gmail.com>
2025-10-06 23:52:19 -0300

    a

--- components/welcome-screen.tsx
+++ components/welcome-screen.tsx
@@ -104,7 +104,7 @@ export default function WelcomeScreen({ onEnterDesktop }: WelcomeScreenProps) {
                 <img
                   src={src}
                   alt=""
-                  className="h-full w-auto block object-cover"
+                  className="h-full w-auto "
                   draggable={false}
                 />
               </div>
