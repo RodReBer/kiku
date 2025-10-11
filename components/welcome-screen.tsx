3a176c6c2c362d3b8a0ac66ad69480d42da4cf70
Rodrigo Rey <rodrigorey2005@gmail.com>
2025-10-11 12:00:45 -0300

    cambios

--- components/welcome-screen.tsx
+++ components/welcome-screen.tsx
@@ -109,7 +109,7 @@ export default function WelcomeScreen({ onEnterDesktop }: WelcomeScreenProps) {
           />
         </div>
         {/* AQUI button wrapper centers translation separate from rotation to avoid jump */}
-  <div className="absolute left-1/2 top-[71%] -translate-x-1/2 ">
+  <div className="absolute left-1/2 top-[70%] -translate-x-1/2 ">
           <motion.img
             src="/inicio/AQUI.svg"
             alt="Aquí"
